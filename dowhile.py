c1f42d0d5219055864b5c82f37cb3ca8e5616e6d
Vad69 <100448054+Vad69@users.noreply.github.com>
2024-10-05 19:29:39 +0530

    first commit

---
+++ dowhile.py
@@ -0,0 +1,11 @@
+# Initialize a variable
+count = 0
+
+# Emulate a do...while loop
+while True:  # Start an infinite loop
+    count += 1  # Perform your operations
+    print("Count:", count)
+    
+    # Condition to exit the loop
+    if count >= 5:
+        break  # Exit the loop
